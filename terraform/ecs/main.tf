ee0c3e6ea033c10ced4f5ced75039938515ffa0d
Cristian Fiori <cris.fiori90@gmail.com>
2023-01-20 23:33:05 -0300

    Trigger the initial pipeline run.

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.13.5"
   backend "remote" {
-    organization = "[Your Org Name]"
+    organization = "test-circleci"
 
     workspaces {
       name = "arm-aws-ecs"
